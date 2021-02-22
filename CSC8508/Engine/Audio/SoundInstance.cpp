dc42d920ed3d9852e7ff984fa04067aea13abca3
Maciej Kawka <46383143+maciejkawka@users.noreply.github.com>
2021-02-22 17:11:23 +0000

    SoundInstace
    All functions implemented

--- CSC8508/Engine/Audio/SoundInstance.cpp
+++ CSC8508/Engine/Audio/SoundInstance.cpp
@@ -4,10 +4,12 @@
 using namespace NCL::CSC8508::Audio;
 
 SoundInstance::SoundInstance():
-	sound(nullptr), audioCore(nullptr), channelID(-1), volume(1), pitch(1), path("")
+	sound(nullptr), audioCore(nullptr), mode(FMOD_3D), channelID(-1), volume(1), pitch(1), path("")
 {
 	attributes3D.pos = ToFMODVECTOR(Maths::Vector3(0,0,0));
 	attributes3D.vel = ToFMODVECTOR(Maths::Vector3(0,0,0));
+	distances.min = 0;
+	distances.max = 0;
 }
 
 SoundInstance::~SoundInstance()
@@ -26,16 +28,13 @@ int SoundInstance::Play()
 	SoundManager::ErrorCheck(audioCore->coreSystem->playSound(sound, 0, true, &channel));
 	if (channel)
 	{
-		FMOD_MODE mode;
-		sound->getMode(&mode);
-
-		/*if (mode & FMOD_3D)
+		if (mode & MODE_3D)
+		{
 			SoundManager::ErrorCheck(channel->set3DAttributes(&attributes3D.pos, &attributes3D.vel));
-		SoundManager::ErrorCheck(channel->setVolume(volume));
+			SoundManager::ErrorCheck(channel->setVolume(volume));
+			SoundManager::ErrorCheck(channel->setPitch(pitch));
+		}
 		SoundManager::ErrorCheck(channel->setPaused(false));
-		SoundManager::ErrorCheck(channel->setPitch(pitch));*/
-		SoundManager::ErrorCheck(channel->setPaused(false));
-
 		audioCore->coreChannels[ChannelID] = channel;
 	}
 	channelID = ChannelID;
@@ -61,6 +60,61 @@ int SoundInstance::isPlaying()
 	return playingState;
 }
 
+void SoundInstance::SetPaused(bool paused)
+{
+	if (!isPlaying())
+		return;
+
+	auto foundChannel = audioCore->coreChannels.find(channelID);
+	SoundManager::ErrorCheck(foundChannel->second->setPaused(paused));
+}
+
+bool SoundInstance::isPaused()
+{
+	if (!isPlaying())
+		return false;
+
+	bool pauseState;
+	auto foundChannel = audioCore->coreChannels.find(channelID);
+	SoundManager::ErrorCheck(foundChannel->second->getPaused(&pauseState));
+	return pauseState;
+}
+
+void SoundInstance::Set3DAttributes(Maths::Vector3 pos, Maths::Vector3 vel)
+{
+	attributes3D.pos = ToFMODVECTOR(pos);
+	attributes3D.vel = ToFMODVECTOR(vel);
+}
+
+void SoundInstance::SetMode(Audio_Mode m)
+{
+	FMOD_MODE fmodMode = FMOD_DEFAULT;
+	if (m & Audio_Mode::MODE_LOOP_ON) fmodMode |= FMOD_LOOP_NORMAL;
+	if (m & Audio_Mode::MODE_LOOP_OFF) fmodMode |= FMOD_LOOP_OFF;
+	if (m & Audio_Mode::MODE_3D) fmodMode |= FMOD_3D;
+	if (m & Audio_Mode::MODE_2D) fmodMode |= FMOD_2D;
+	SoundManager::ErrorCheck(sound->setMode(fmodMode));
+	mode = (int)m;
+}
+
+void SoundInstance::SetMode(int m)
+{
+	FMOD_MODE fmodMode = FMOD_DEFAULT;
+	if (m & Audio_Mode::MODE_LOOP_ON) fmodMode |= FMOD_LOOP_NORMAL;
+	if (m & Audio_Mode::MODE_LOOP_OFF) fmodMode |= FMOD_LOOP_OFF;
+	if (m & Audio_Mode::MODE_3D) fmodMode |= FMOD_3D;
+	if (m & Audio_Mode::MODE_2D) fmodMode |= FMOD_2D;
+	SoundManager::ErrorCheck(sound->setMode(fmodMode));
+	mode = m;
+}
+
+void SoundInstance::SetMaxMinDistance(float max, float min)
+{
+	distances.max = max;
+	distances.min = min;
+	SoundManager::ErrorCheck(sound->set3DMinMaxDistance(min, max));
+}
+
 FMOD_VECTOR SoundInstance::ToFMODVECTOR(const Maths::Vector3& v)
 {
 	FMOD_VECTOR vec;
@@ -69,3 +123,4 @@ FMOD_VECTOR SoundInstance::ToFMODVECTOR(const Maths::Vector3& v)
 	vec.z = v.z;
 	return vec;
 }
+
--- CSC8508/Engine/Audio/main.cpp
+++ CSC8508/Engine/Audio/main.cpp
@@ -4,27 +4,33 @@
 using namespace NCL::CSC8508;
 int main()
 {
-
-	//FMOD_RESULT result;
-	//FMOD::System* system = nullptr;
-	//result = FMOD::System_Create(&system);
-	//result = system->init(512, FMOD_INIT_NORMAL, 0);
-
-	//FMOD::Sound* sound;
-
-	//system->createSound("res/Dogs_bark_long.wav", FMOD_DEFAULT, 0, &sound);
-	//system->playSound(sound, 0, false, 0);
-	//while (true);
 	Audio::SoundManager::Init();
-	Audio::SoundInstance test;
-	Audio::SoundManager::CreateInstance("res/Dogs_bark_long.wav", &test);
-	test.Play();
-	Audio::SoundInstance test1;
-	Audio::SoundManager::CreateInstance("res/River.mp3", &test1);
-	test1.Play();
-	Audio::SoundInstance test2;
-	Audio::SoundManager::CreateInstance("res/Dogs_bark_long.wav", &test2);
-	test2.Play();
+
+	Audio::SoundInstance* test = new Audio::SoundInstance();
+	Audio::SoundManager::CreateInstance("res/Dogs_bark_long.wav", test);
+	test->Play();
+
+	Audio::SoundInstance* test1 = new Audio::SoundInstance();;
+	Audio::SoundManager::CreateInstance("res/River.mp3", test1);
+
+	test1->SetMode(Audio::MODE_3D | Audio::MODE_LOOP_ON);
+	test1->SetPitch(0.5f);
+	test1->SetVolume(1.0f);
+	test1->Play();
+	bool is = test1->isPaused();
+	test1->SetPaused(true);
+	is = test1->isPaused();
+
+	Audio::SoundInstance* test2 = new Audio::SoundInstance();
+	Audio::SoundManager::CreateInstance("res/Dogs_bark_long.wav", test2);
+	//test2->Play();
+
+	delete test2;
+	delete test;
     Audio::SoundManager::Update();
-	while (true);
+	test1->SetPaused(false);
+
+	while (true)
+		std::cout << test1->isPlaying() << std::endl;
+	
  }
