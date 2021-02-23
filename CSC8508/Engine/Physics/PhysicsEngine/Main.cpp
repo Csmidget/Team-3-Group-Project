ef0c2814331461d1a7d908dd4e5d14e77bf35e7d
Charles Smith <charles@minidev.co.uk>
2021-02-23 15:53:01 +0000

    Fixed bullet weirdness

--- CSC8508/Engine/Physics/PhysicsEngine/Main.cpp
+++ CSC8508/Engine/Physics/PhysicsEngine/Main.cpp
@@ -15,7 +15,6 @@ subject to the following restrictions:
 
 ///-----includes_start-----
 #include "btBulletDynamicsCommon.h"
-
 #include <stdio.h>
 
 /// This is a Hello World program for running a basic Bullet physics simulation
