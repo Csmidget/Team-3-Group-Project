69c38fd1d194a0216f6ff3997d47e3b7ca8603de
1JiachenXie <74049880+1JiachenXie@users.noreply.github.com>
2021-02-24 14:50:49 +0000

    Lighting staff

---
+++ CSC8508/Game/Light.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+
+#include "../../Common/Vector3.h"
+#include "../../Common/Vector4.h"
+
+
+
+class Light
+{
+public:
+	Light() {}
+
+};
+
