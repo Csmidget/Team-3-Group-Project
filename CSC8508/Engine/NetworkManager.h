7c6a0387aa2871f02b7333393b1c974f0732a335
ejgoodchild <32480163+ejgoodchild@users.noreply.github.com>
2021-03-04 17:26:37 +0000

    Intergrated NetworkGame into NetworkManager

--- CSC8508/Engine/NetworkManager.h
+++ CSC8508/Engine/NetworkManager.h
@@ -4,14 +4,14 @@
 #include "../Engine/NetworkObject.h"
 #include "../Engine/NetworkBase.h"
 
-using namespace NCL;
-using namespace CSC8508;
+
 using namespace std;
 
 #include <chrono>
 #include <thread>
 #include <string>
 #include <iostream>
+#include "../Game/NetworkPlayer.h"
 
 class TestPacketReceiver : public PacketReceiver {
 public:
@@ -29,19 +29,49 @@ public:
 	}
 	string name;
 };
+namespace NCL {
+	namespace CSC8508 {
+		class NetworkManager : public PacketReceiver
+		{
 
-class NetworkManager
-{
+		public:
+			NetworkManager();
+			~NetworkManager();
 
-	public:
-		NetworkManager();
-		~NetworkManager();
-		void Update(float dt);
-	private:
-		void TestClient();
-		void TestServer();
+			void Update(float dt);
 
-		bool const OFFLINE_MODE = false;
-		bool isClient = false;
-};
+			void ReceivePacket(int type, GamePacket* payload, int source) override;
+			void OnPlayerCollision(NetworkPlayer* a, NetworkPlayer* b);
 
+		private:
+			void TestClient();
+			void TestServer();
+
+			void StartAsServer();
+			void StartAsClient();
+
+			void UpdateAsServer(float dt);
+			void UpdateAsClient(float dt);
+
+			void BroadcastSnapshot(bool deltaFrame);
+			void UpdateMinimumState();
+
+
+
+			bool const OFFLINE_MODE = true;
+			bool const TEST_MODE = true;
+			bool isClient = true;
+
+			GameServer* thisServer;
+			GameClient* thisClient;
+			float timeToNextPacket;
+			int packetsToSnapshot;
+
+			std::vector<NetworkObject*> networkObjects;
+
+			std::map<int, GameObject*> serverPlayers;
+			GameObject* localPlayer;
+		};
+
+	}
+}
