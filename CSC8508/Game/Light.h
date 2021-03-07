646c2480bd12b9ca16911bde1edfd53bf46b3fc3
1JiachenXie <74049880+1JiachenXie@users.noreply.github.com>
2021-03-07 23:36:33 +0000

    light staff changes

--- CSC8508/Game/Light.h
+++ CSC8508/Game/Light.h
@@ -1,5 +1,64 @@
 #pragma once
-class Light
-{
-};
+#include"../../Common/Vector3.h"
+#include"../../Common/Vector4.h"
+#include"../Game/GameTechRenderer.h"
+#include "../Engine/GameWorld.h"
+
+#include "../../Plugins/OpenGLRendering/OGLRenderer.h"
+#include "../../Plugins/OpenGLRendering/OGLShader.h"
+#include "../../Plugins/OpenGLRendering/OGLTexture.h"
+#include "../../Plugins/OpenGLRendering/OGLMesh.h"
+
+#include "../../Common/ResourceManager.h"
+
+
+namespace NCL {
+	class Maths::Vector3;
+	class Maths::Vector4;
+
+	namespace CSC8508 {
+		class Light
+		{
+		public:
+			Light() {}
+			Light(const Vector3& position, const Vector4& colour, float radius)
+			{
+				this->position = position;
+				this->colour = colour;
+				this->radius = radius;
+			}
+
+			~Light(void) {}
+
+			Vector3 GetPosition() const { return position; }
+			void SetPosition(const Vector3& val) { position = val; }
+
+			float GetRadius() const { return radius; }
+			void SetRadius(float val) { radius = val; }
+
+			Vector4 GetColour() const { return colour; }
+			void SetColour(const Vector4& val) { colour = val; }
+
+		protected:
+			Vector3 position;
+			float radius;
+			Vector4 colour;
+
+
+		};
+
+
+
+
+
+
+
+
+
+	}
+
+}
+
+
+
 
