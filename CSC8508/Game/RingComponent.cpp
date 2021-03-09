e27c1c2d991cb29fec3869a9350480a9297312f4
DweltOggy <C.Oghoetuoma2@newcastle.ac.uk>
2021-03-09 09:37:50 +0000

    minor changes

--- CSC8508/Game/RingComponent.cpp
+++ CSC8508/Game/RingComponent.cpp
@@ -6,14 +6,17 @@ using namespace CSC8508;
 
 RingComponent::RingComponent(GameObject* object) : Component(object)
 {
+	active = true;
 	physicsObject = object->GetPhysicsObject();
 	physicsObject->body->makeTrigger();
 }
 
 void RingComponent::OnCollisionBegin(GameObject* other)
 {
-	if (other->HasTag("Player"))
+	if (other->HasTag("Player") && active)
 	{
+		//add score
 
+		active = false;
 	}
 }
