2969e061877dc20d83e03a8f4185270f89d8506c
unknown <1261608276@qq.com>
2021-03-16 18:57:21 +0800

    shadow

--- Assets/Shaders/GameTechShadowFrag.glsl
+++ Assets/Shaders/GameTechShadowFrag.glsl
@@ -2,7 +2,11 @@
 
 out vec4 fragColor;
 
+in vec4 FragPos;
+uniform float farPlane;
+uniform vec3 lightPos;
 void main(void)
 {
-	fragColor = vec4(1,1,1,1);
+    fragColor = vec4 (1,1,1,1);
+    gl_FragDepth = length(lightPos-FragPos.xyz)/farPlane;
 }
