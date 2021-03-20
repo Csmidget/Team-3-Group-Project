2759073784134c51f446906f13525855ae8fe17c
Maciej Kawka <46383143+maciejkawka@users.noreply.github.com>
2021-03-20 13:51:12 +0000

    TimeScore Updated

--- CSC8508/Game/TimeScoreComponent.cpp
+++ CSC8508/Game/TimeScoreComponent.cpp
@@ -1,34 +1,22 @@
 #include"TimeScoreComponent.h"
 
-NCL::CSC8508::TimeScoreComponent::TimeScoreComponent(GameObject* object) : Component("TimeScoreComponent", object)
+using namespace NCL;
+using namespace CSC8508;
+
+TimeScoreComponent::TimeScoreComponent(GameObject* object)
+	: Component("TimeScoreComponent", object)
 {
-	timeScore = 0;
-	nowTime = 0;
-	nextTime = 0;
-	num = 1;
+	timeScore = 0.0f;
 }
 
-int NCL::CSC8508::TimeScoreComponent::UpdateScore(float dt)
+void TimeScoreComponent::Update(float dt)
 {
-	getTime.Tick();
-
-	if (num == 1) {
-		nowTime = getTime.GetTotalTimeSeconds();
-		num = 0;
-	}
-	else {
-		nextTime = getTime.GetTotalTimeSeconds();
-		if (nextTime - nowTime == 1) {
-			timeScore += 1;
-			num = 1;
-		}
-		else if (nextTime - nowTime > 1) {
-			num = 1;
-		}
-		else {
-			num = 0;
-		}
-		return timeScore;
-	}
-
+	timeScore += dt;
 }
+
+int TimeScoreComponent::GetScore() const
+{
+	return (int)timeScore;
+}
+
+
