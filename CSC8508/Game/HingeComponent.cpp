599fb38a0a895d141b5aa3e5718ec0026ba91470
Maciej Kawka <46383143+maciejkawka@users.noreply.github.com>
2021-03-22 09:21:57 +0000

    Hinge Component Implemented

---
+++ CSC8508/Game/HingeComponent.cpp
@@ -0,0 +1,15 @@
+#pragma once
+#include"HingeComponent.h"
+#include"Game.h"
+#include"../Engine/Physics/PhysicsEngine/BulletWorld.h"
+
+using namespace NCL;
+using namespace CSC8508;
+
+HingeComponent::HingeComponent(GameObject* object, Game* game, Maths::Vector3 point, Maths::Vector3 axis)
+	:Component("HingeComponent", object)
+{
+	game->GetPhysics()->addhingeconstraint(object->GetPhysicsObject()->body, point, axis);
+}
+
+
--- CSC8508/Game/JSONComponentFactory.cpp
+++ CSC8508/Game/JSONComponentFactory.cpp
@@ -14,6 +14,7 @@
 #include "GameStateManagerComponent.h"
 #include"DisappearingPlatformComponent.h"
 #include"TimeScoreComponent.h"
+#include"HingeComponent.h"
 
 using namespace NCL;
 using namespace CSC8508;
@@ -28,6 +29,7 @@ Component* JSONComponentFactory::AddComponentFromJson(json componentJson, GameOb
 	//if (name == "Bonus")		return gameObject->AddComponent<BonusComponent>(componentJson["reward"]);
 	if (name == "DisappearingPlatform")			return gameObject->AddComponent<DisappearingPlatformComponent>();
 	if (name == "GameStateManager")		return gameObject->AddComponent<GameStateManagerComponent>(game->GetWorld());
+	if (name == "HingeComponent")		return gameObject->AddComponent<HingeComponent>(game, JsonToVector3(componentJson["point"]), JsonToVector3(componentJson["axis"]));
 	if (name == "Player")		return gameObject->AddComponent<PlayerComponent>(game);
 	if (name == "PlaySound")	return gameObject->AddComponent<PlaySound>(componentJson["path"], componentJson["mode"], componentJson["volume"], componentJson["min"]);
 	if (name == "Respawn")		return gameObject->AddComponent<RespawnComponent>();
