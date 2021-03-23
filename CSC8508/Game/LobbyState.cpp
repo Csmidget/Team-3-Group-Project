3ba7f06f806c174309ab5680668c3097bdb2165e
Maciej Kawka <46383143+maciejkawka@users.noreply.github.com>
2021-03-23 13:40:32 +0000

    Update LobbyState.cpp

--- CSC8508/Game/LobbyState.cpp
+++ CSC8508/Game/LobbyState.cpp
@@ -25,7 +25,7 @@ PushdownState::PushdownResult LobbyState::OnUpdate(float dt, PushdownState** new
 	//Something needs to be checked here to know when to start the game. For now we just wait for K to be pressed.
 	if (game->IsExitLobbyTime()) {
 		gameStarted = true;
-		*newState = new PlayState(game);
+		*newState = new PlayState(game, true);
 		game->InitNetworkPlayers();
 		return PushdownResult::Push;
 	}
