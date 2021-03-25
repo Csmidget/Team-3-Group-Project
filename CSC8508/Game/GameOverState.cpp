c174a7f069b947781ed672919afc5d9bcdac87e9
Charles Smith <charles@minidev.co.uk>
2021-03-25 10:18:23 +0000

    gameover screen tweak

--- CSC8508/Game/GameOverState.cpp
+++ CSC8508/Game/GameOverState.cpp
@@ -49,7 +49,7 @@ PushdownState::PushdownResult GameOverState::OnUpdate(float dt, PushdownState** 
 		if (allFinished)
 		{
 			timer -= dt;
-			game->getRenderer()->DrawString("Next Level: " + std::to_string((int)timer), Vector2(40, 95), Vector4(1.0f, 1.0f, 0.0f, 0.0f), 15.0f);
+			game->getRenderer()->DrawString("Next Level: " + std::to_string((int)timer), Vector2(43, 95), Vector4(1.0f, 1.0f, 0.0f, 0.0f), 15.0f);
 		}
 		else
 			game->getRenderer()->DrawString("Waiting for other players!", Vector2(30, 95), Vector4(1.0f, 1.0f, 0.0f, 0.0f), 15.0f);
